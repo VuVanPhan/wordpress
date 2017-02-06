b363a0fba0e015c1a6ca8454243c9d30cfa39400
Phan <vanphanbk1407@gmail.com>
2017-02-06 14:52:54 +0700

    woocommerce commit

---
+++ wp-content/plugins/module-first/module-first.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * @package Module First
+ */
+/*
+Plugin Name: Module First
+Plugin URI: http://localhost/wordpress/
+Description: Demo module first for wordpress
+Version: 1.0
+Author: Magestore
+Author URI: http://www.magestore.com/
+License: GPLv2 or later
+Text Domain: module-first
+*/
